fd31533f6df3bf05b24b1be16ac58a83deaa9842
Luana <luanaonp@gmail.com>
2022-06-18 22:16:46 -0300

    Special move - En Passant

--- src/application/UI.java
+++ src/application/UI.java
@@ -121,7 +121,7 @@ public class UI {
 		System.out.print(ANSI_RESET);
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
-		System.out.println(Arrays.toString(white.toArray()));
+		System.out.println(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
 		
 		
